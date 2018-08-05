5c7c478ea6a06bf0e96013b5037aabc6d2a2b493
Nagarjuna Pamidi <mr_pamidi@Nagarjunas-MacBook-Pro.local>
2018-08-04 20:27:24 -0400

    add .ppm file store

--- capture.cpp
+++ capture.cpp
@@ -2,9 +2,11 @@
 #include "capture.hpp"
 
 //global variables
-extern pthread_cond_t cond_capture_thread;
+extern pthread_cond_t cond_query_frames_thread;
 extern pthread_mutex_t system_time_mutex_lock;
 extern unsigned long long system_time;
+
+pthread_cond_t cond_store_frames = PTHREAD_COND_INITIALIZER;
 
 using namespace cv;
 using namespace std;
@@ -25,42 +27,45 @@ void *query_frames(void *cameraIdx)
     cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, FRAME_HRES);
     cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, FRAME_VRES);
     cvNamedWindow(capture_window_title, CV_WINDOW_AUTOSIZE);
-    
+
 
     while(1)
     {
-                
+
         pthread_mutex_lock(&system_time_mutex_lock);
         //wait for signal
-        pthread_cond_wait(&cond_capture_thread, &system_time_mutex_lock);
+        pthread_cond_wait(&cond_query_frames_thread, &system_time_mutex_lock);
         pthread_mutex_unlock(&system_time_mutex_lock);
 
         #ifdef DEBUG_MODE_ON
-            syslog(LOG_WARNING," cvQueryframe start at :%lld", system_time);   
+            syslog(LOG_WARNING," cvQueryframe start at :%lld", system_time);
         #endif
-        
+
         //thread safe
-        pthread_mutex_lock(&frame_mutex_lock);                
+        pthread_mutex_lock(&frame_mutex_lock);
         frame = cvQueryFrame(capture);
-        pthread_mutex_unlock(&frame_mutex_lock);
-        
+		pthread_cond_signal(&cond_store_frames); //signal to store current file..
+		pthread_mutex_unlock(&frame_mutex_lock);
+
         if(!frame) break;
-        
+
         #ifdef DEBUG_MODE_ON
             syslog(LOG_WARNING," cvQueryframe done at :%lld", system_time);
         #endif
-        
+
         cvShowImage(capture_window_title, frame);
-            
+
         char c = cvWaitKey(1);
         if( c == 'q')
         {
             break;
         }
-        
+
         #ifdef DEBUG_MODE_ON
             syslog(LOG_WARNING," cvShowImage done at :%lld", system_time);
-        #endif        
+        #endif
+
+		break;
     }
 
     cvReleaseCapture(&capture);
@@ -69,26 +74,35 @@ void *query_frames(void *cameraIdx)
     #ifdef DEBUG_MODE_ON
         syslog(LOG_WARNING," query_frames() exiting...");
     #endif
-        								
+
 };
 
 
 void *store_frames(void *params)
 {
-        
+
     vector<int> compression_params;
-    compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
-    compression_params.push_back(9);
-    
-    Mat mat = cvarrToMat(frame);
-    
-    try
-    {
-        imwrite("alpha.png", mat, compression_params);
-    }
-    catch (runtime_error& ex) 
-    {
-        syslog(LOG_ERR, " ***Exception converting image to PNG format:");
-        exit(ERROR);
-    }
+    compression_params.push_back(CV_IMWRITE_PXM_BINARY);
+    compression_params.push_back(1);
+
+    Mat mat;
+	while(1)
+	{
+		pthread_mutex_lock(&frame_mutex_lock);
+		pthread_cond_wait(&cond_store_frames, &frame_mutex_lock);
+        pthread_mutex_unlock(&frame_mutex_lock);
+
+		mat = cvarrToMat(frame);
+
+	   try
+	   {
+		   imwrite("alpha.ppm", mat, compression_params);
+	   }
+	   catch (runtime_error& ex)
+	   {
+		   syslog(LOG_ERR, " ***Exception converting image to PNG format:");
+		   exit(ERROR);
+	   }
+	}
+
 }
--- main.c
+++ main.c
@@ -13,14 +13,16 @@
 #define THREAD1         (1)
 #define THREAD2         (2)
 #define THREAD3         (3)
-#define CAPTURE_THREAD  (4)
+#define QUERY_FRAMES_THREAD  	(4)
+#define STORE_FRAMES_THREAD		(5)
 
 //Note: In this application, 0 is the RT MAX priority..
 //..the assign_RT_scheduler_attr() function internally converts 0 to RT_MAX, and similar
 #define THREAD1_PRIORITY        (SCHED_FIFO_MAX_PRIORITY + 10) //used as (sched_get_priority_max(SCHED_FIFO) - (SCHED_FIFO_MAX_PRIORITY + 10))
 #define THREAD2_PRIORITY        (SCHED_FIFO_MAX_PRIORITY + 10) //used as (sched_get_priority_max(SCHED_FIFO) - (SCHED_FIFO_MAX_PRIORITY + 10))
 #define THREAD3_PRIORITY        (SCHED_FIFO_MAX_PRIORITY + 10) //used as (sched_get_priority_max(SCHED_FIFO) - (SCHED_FIFO_MAX_PRIORITY + 10))
-#define CAPTURE_THREAD_PRIORITY (SCHED_FIFO_MAX_PRIORITY + 1)  //sed as (sched_get_priority_max(SCHED_FIFO) - (SCHED_FIFO_MAX_PRIORITY + 1))
+#define QUERY_FRAMES_THREAD_PRIORITY	(SCHED_FIFO_MAX_PRIORITY + 1)  //used as (sched_get_priority_max(SCHED_FIFO) - (SCHED_FIFO_MAX_PRIORITY + 1))
+#define STORE_FRAMES_THREAD_PRIORITY	(SCHED_FIFO_MAX_PRIORITY + 2)  //used as (sched_get_priority_max(SCHED_FIFO) - (SCHED_FIFO_MAX_PRIORITY + 2))
 
 //global time variable, and a mutex to restrict access to it
 unsigned long long system_time = 0;
@@ -29,7 +31,7 @@ pthread_mutex_t system_time_mutex_lock;
 pthread_cond_t cond_thread1 = PTHREAD_COND_INITIALIZER;
 pthread_cond_t cond_thread2 = PTHREAD_COND_INITIALIZER;
 pthread_cond_t cond_thread3 = PTHREAD_COND_INITIALIZER;
-pthread_cond_t cond_capture_thread = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_query_frames_thread = PTHREAD_COND_INITIALIZER;
 
 //function prototyping
 void *print_test(void *threadIdx);
@@ -71,42 +73,45 @@ int main( int argc, char** argv )
 void *thread_dispatcher(void *something)
 {
     int rc;
-    pthread_t thread1, thread2, thread3, capture_thread;
-    pthread_attr_t thread1_attr, thread2_attr, thread3_attr, capture_thread_attr;
-    threadParams_t thread1Idx, thread2Idx, thread3Idx, capture_threadIdx;
-    struct sched_param thread1_sched_param, thread2_sched_param, thread3_sched_param, capture_thread_sched_param;
+    pthread_t thread1, thread2, thread3, query_frames_thread, store_frames_thread;
+    pthread_attr_t thread1_attr, thread2_attr, thread3_attr, query_frames_thread_attr, store_frames_thread_attr;
+    threadParams_t thread1Idx, thread2Idx, thread3Idx, query_frames_threadIdx, store_frames_threadIdx;
+    struct sched_param thread1_sched_param, thread2_sched_param, thread3_sched_param, query_frames_thread_sched_param, store_frames_thread_sched_param;
+
     //posix timer parameters
     struct sigevent sigevent_param;
     timer_t timer_id;
     struct itimerspec timer_period;
     pthread_attr_t timer_thread_attr;
     struct sched_param timer_thread_sched_param;
-    
-    
+
+
     thread1Idx.threadIdx = 1;
     thread2Idx.threadIdx = 2;
     thread3Idx.threadIdx = 3;
-    capture_threadIdx.threadIdx = 4;
+    query_frames_threadIdx.threadIdx = 4;
+	store_frames_threadIdx.threadIdx = 5;
 
     //assign RT scheduler attributes
     assign_RT_schedular_attr(&thread1_attr, &thread1_sched_param, SCHED_FIFO, THREAD1_PRIORITY, (jetson_tx2_cores)JETSON_TX2_ARM_CORE2);
     assign_RT_schedular_attr(&thread2_attr, &thread2_sched_param, SCHED_FIFO, THREAD2_PRIORITY, (jetson_tx2_cores)JETSON_TX2_ARM_CORE2);
     assign_RT_schedular_attr(&thread3_attr, &thread3_sched_param, SCHED_FIFO, THREAD3_PRIORITY, (jetson_tx2_cores)JETSON_TX2_ARM_CORE2);
-    assign_RT_schedular_attr(&capture_thread_attr, &capture_thread_sched_param, SCHED_FIFO, CAPTURE_THREAD_PRIORITY, (jetson_tx2_cores)JETSON_TX2_ARM_CORE2);
+    assign_RT_schedular_attr(&query_frames_thread_attr, &query_frames_thread_sched_param, SCHED_FIFO, QUERY_FRAMES_THREAD_PRIORITY, (jetson_tx2_cores)JETSON_TX2_ARM_CORE2);
+	assign_RT_schedular_attr(&store_frames_thread_attr, &store_frames_thread_sched_param, SCHED_FIFO, STORE_FRAMES_THREAD_PRIORITY, (jetson_tx2_cores)JETSON_TX2_ARM_CORE2);
 
 #ifdef DEBUG_MODE_ON
     syslog_scheduler();
 #endif
 
-//initialize timer thread attributes 
+//initialize timer thread attributes
     assign_RT_schedular_attr(&timer_thread_attr, &timer_thread_sched_param, SCHED_FIFO, SCHED_FIFO_MAX_PRIORITY, (jetson_tx2_cores)JETSON_TX2_ARM_CORE2);
 
-//assign sigevent paramaters for the timer  
+//assign sigevent paramaters for the timer
     sigevent_param.sigev_notify = SIGEV_THREAD;
     sigevent_param.sigev_value.sival_ptr = &timer_id;
     sigevent_param.sigev_notify_function = &timer_handler;
     sigevent_param.sigev_notify_attributes = &timer_thread_attr;
-    
+
 //initialize timer period values
     timer_period.it_value.tv_sec = (APP_TIMER_INTERVAL_IN_MSEC / MSEC_PER_SEC);
     timer_period.it_value.tv_nsec = (APP_TIMER_INTERVAL_IN_MSEC * NSEC_PER_MSEC);
@@ -120,12 +125,12 @@ void *thread_dispatcher(void *something)
 //initilize mutex to protect timer count variable
     rc = pthread_mutex_init(&system_time_mutex_lock, NULL);
     assert(rc == 0);
-    
+
 //start timer
     rc = timer_settime(timer_id, 0, &timer_period, 0);
     assert (rc == 0);
-    
-//create threads to test timer  
+
+//create threads to test timer
     syslog(LOG_WARNING,"\n Thread:1 dispatching with priority ==> %d <==", thread1_sched_param.sched_priority);
     pthread_create(&thread1, &thread1_attr, print_test, (void *)&thread1Idx );
 
@@ -134,13 +139,15 @@ void *thread_dispatcher(void *something)
 
     syslog(LOG_WARNING,"\n Thread:3 dispatching with priority ==> %d <==", thread3_sched_param.sched_priority);
     pthread_create(&thread3, &thread3_attr, print_test, (void *)&thread3Idx );
-    
-    syslog(LOG_WARNING,"\n Capture Thread dispatching with priority ==> %d <==", capture_thread_sched_param.sched_priority);
-    pthread_create(&capture_thread, &capture_thread_attr, query_frames, (void *)&capture_threadIdx );
 
-    
-    pthread_join(capture_thread, NULL);
-    
+    syslog(LOG_WARNING,"\n QUERY_FRAMES_THREAD dispatching with priority ==> %d <==", query_frames_thread_sched_param.sched_priority);
+    pthread_create(&query_frames_thread, &query_frames_thread_attr, query_frames, (void *)&query_frames_threadIdx);
+
+	syslog(LOG_WARNING,"\n Store frames Thread dispatching with priority ==> %d <==", query_frames_thread_sched_param.sched_priority);
+	pthread_create(&store_frames_thread, &store_frames_thread_attr, store_frames, (void *)&store_frames_threadIdx);
+
+    pthread_join(query_frames_thread, NULL);
+
     //wait for threads to join
     //pthread_join( thread1, NULL);
     //pthread_join( thread2, NULL);
@@ -152,11 +159,11 @@ void *thread_dispatcher(void *something)
     rc = timer_settime(timer_id, 0, &timer_period, 0);
     assert (rc == 0);
 
-//destroy mutex lock        
+//destroy mutex lock
     pthread_mutex_destroy(&system_time_mutex_lock);
 
     syslog(LOG_WARNING," RT Dispatcher thread exiting...");
-    
+
     pthread_exit(NULL);
 }
 
@@ -166,34 +173,34 @@ void *thread_dispatcher(void *something)
 void timer_handler(union sigval arg)
 {
     pthread_mutex_lock(&system_time_mutex_lock);
-    
+
     system_time += APP_TIMER_INTERVAL_IN_MSEC; //update time periodically
-    
+
     if(((system_time) % (MSEC_PER_SEC*7)) == 0)
     {
         pthread_cond_signal(&cond_thread1);
     }
-    
+
     if(((system_time) % (MSEC_PER_SEC*3)) == 0)
     {
         pthread_cond_signal(&cond_thread2);
     }
-    
+
     if(((system_time) % (MSEC_PER_SEC*5)) == 0)
     {
         pthread_cond_signal(&cond_thread3);
     }
-    
+
     //run at 30Hz
     if((system_time % QUERY_FRAMES_INTERVAL_IN_MSEC) == 0)
     {
         //signal capture thread..
-        pthread_cond_signal(&cond_capture_thread);
+        pthread_cond_signal(&cond_query_frames_thread);
     }
-    
+
     pthread_mutex_unlock(&system_time_mutex_lock);
 
-    //syslog(LOG_WARNING, " timer_handler called at %lld", system_time);    
+    //syslog(LOG_WARNING, " timer_handler called at %lld", system_time);
 }
 
 
@@ -209,61 +216,61 @@ void *print_test(void *threadIdx)
 
     sleep_delta_time.tv_sec = 0;
     sleep_delta_time.tv_nsec = (APP_TIMER_INTERVAL_IN_MSEC * NSEC_PER_MSEC);
-    
-    
+
+
     while(1)
     {
         //yield before proceeding further
         //so that other thread will get a chance to run as well..
         //pthread_yield();
-        
+
         pthread_mutex_lock(&system_time_mutex_lock);
-    
-    
+
+
         if(currentIdx->threadIdx == THREAD1) //high priority task
         {
             pthread_cond_wait(&cond_thread1, &system_time_mutex_lock);
-            
+
             //if((system_time % (30)) == 0)
             {
                 #ifdef DEBUG_MODE_ON
 //                syslog(LOG_WARNING," Thread:%d, timer:%lld", (int)currentIdx->threadIdx, system_time);
                 #endif
-                
+
                 //if((loopCounter++) >= 10) break;
                 //rc = nanosleep(&sleep_delta_time, NULL);
-                //assert(rc == 0);                
+                //assert(rc == 0);
             }
-            
+
         }
 
         else if(currentIdx->threadIdx == THREAD2) //mid priority thread
         {
             pthread_cond_wait(&cond_thread2, &system_time_mutex_lock);
-            
+
             //if((system_time % (20)) == 0)
             {
                 #ifdef DEBUG_MODE_ON
 //                    syslog(LOG_WARNING," Thread:%d, timer:%lld", (int)currentIdx->threadIdx, system_time);
-                #endif                
+                #endif
                 //if((loopCounter++) >= 10) break;
                 //rc = nanosleep(&sleep_delta_time, NULL);
-                //assert(rc == 0);  
+                //assert(rc == 0);
             }
         }
 
         else if(currentIdx->threadIdx == THREAD3) //low priority thread
         {
             pthread_cond_wait(&cond_thread3, &system_time_mutex_lock);
-            
+
             //if((system_time % (10)) == 0)
             {
                 #ifdef DEBUG_MODE_ON
 //                    syslog(LOG_WARNING," Thread:%d, timer:%lld", (int)currentIdx->threadIdx, system_time);
                 #endif
-                //if((loopCounter++) >= 10) break;  
+                //if((loopCounter++) >= 10) break;
                 //rc = nanosleep(&sleep_delta_time, NULL);
-                //assert(rc == 0);              
+                //assert(rc == 0);
             }
         }
 
@@ -272,7 +279,7 @@ void *print_test(void *threadIdx)
             printf("\nUn-known thread!");
             exit(-1);
         }
-        
+
         pthread_mutex_unlock(&system_time_mutex_lock);
     }
 
